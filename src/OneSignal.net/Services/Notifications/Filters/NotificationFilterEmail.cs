9ebe9466564d150b1bf190dfb513a05465f48971
Donald Slagle <donald.slagle@evomobius.com>
2021-01-03 21:57:40 -0500

    Add initial code

---
+++ src/OneSignal.net/Services/Notifications/Filters/NotificationFilterEmail.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace OneSignal {
+    public class NotificationFilterEmail : NotificationFilter {
+        public NotificationFilterEmail() {
+            Field = "email";
+        }
+
+        /// <summary>
+        /// Only for sending Push Notifications. To send emails to specific email addresses use IncludeEmailTokens parameter.
+        /// </summary>
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
